1911d7923f1977205bd88a97d01abd3a5e27b2e7
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-01-24 12:03:02 +0000

    combine v1 action and tested happy path

--- app/controllers/v1/secrets-controller.js
+++ app/controllers/v1/secrets-controller.js
@@ -1,13 +1,16 @@
 // v1/secrets-controller
 
 const { body, validationResult } = require('express-validator/check')
-const secrets = require('dark-crystal-secrets')
+const secrets = require('dark-crystal-secrets/v1')
 
 exports.share = (req, res, next) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() })
+    return res
+      .status(422)
+      .json({ errors: errors.array() })
+
   } else {
     const params = req.body
 
@@ -17,12 +20,29 @@ exports.share = (req, res, next) => {
       parseInt(params.quorum)
     )
 
-    res.json({ shards })
+    return res
+      .status(201)
+      .json({ shards })
   }
 }
 
 exports.combine = (req, res, next) => {
-  next()
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ errors: errors.array() })
+
+  } else {
+    const params = req.body
+
+    const secret = secrets.combine(params.shards)
+
+    return res
+      .status(201)
+      .json({ secret })
+  }
 }
 
 exports.validate = (method) => {
@@ -32,17 +52,21 @@ exports.validate = (method) => {
     body('shards', "shards doesn't exist").exists().matches(/^[0-9]+$/).custom(greaterThanOrEqualToQuorum),
   ]
 
-  const combine = []
+  const combine = [
+    body('shards', "shards doesn't exist").exists().isArray(),
+    body('shards', "shards are not valid").custom(validShards)
+  ]
 
   return { share, combine }[method]
 
   function greaterThanOrEqualToQuorum (value, action) {
     return parseInt(value) >= parseInt(action.req.body.quorum)
   }
+
+  function validShards (array, action) {
+    return array
+      .map(secrets.validateShard)
+      .filter(Boolean)
+      .length === array.length
+  }
 }
-    // body('version', "version doesn't exist").exists().matches(/^[0-9]+\.[0-9]+\.[0-9]+$/),
-    // body('label', "label doesn't exist").custom(permitV1)
-  // function permitV1 (value, action) {
-  //   if (action.req.body.version === '1.0.0') return true
-  //   else value && 'string' === typeof value
-  // }
--- test/v1/secrets/combine.test.js
+++ test/v1/secrets/combine.test.js
@@ -12,7 +12,11 @@ describe('POST /v1/secrets/combine', (context) => {
     request = supertest.agent(server)
 
     params = {
-      shares: ['a','b','c']
+      shards: [
+        '80104424cf070d06548da4dd40d47f4c156',
+        '802a274f51b2fead5dd92a240a5f5545e67',
+        '803a636b9ea5f4eb0f0489d94cbb2c59f42'
+      ]
     }
   })
 
