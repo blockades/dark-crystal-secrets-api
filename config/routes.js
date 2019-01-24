60ee8fc5a5ba06278b9fe8e840b55a1dd2ae9c7c
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-01-24 16:34:07 +0000

    add routes for v2

--- config/routes.js
+++ config/routes.js
@@ -19,5 +19,30 @@ module.exports = (router, controllers) => {
     v1.shards.validate('verify'),
     v1.shards.verify
   )
+
+  // V2 Routes
+  router.post(
+    '/v2/secrets/share',
+    v2.secrets.validate('share'),
+    v2.secrets.share
+  )
+
+  router.post(
+    '/v2/secrets/combine',
+    v2.secrets.validate('combine'),
+    v2.secrets.combine
+  )
+
+  router.post(
+    '/v2/secrets/verify',
+    v2.secrets.validate('verify'),
+    v2.secrets.verify
+  )
+
+  router.post(
+    '/v2/shards/verify',
+    v2.shards.validate('verify'),
+    v2.shards.verify
+  )
 }
 
