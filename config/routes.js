b73f18f5eab91346b6b56c6f191cd6c4b0868fd5
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-01-25 11:00:34 +0000

    change routes to reflect requiring version as a param

--- config/routes.js
+++ config/routes.js
@@ -1,48 +1,7 @@
 module.exports = (router, controllers) => {
-  const { v1, v2 } = controllers
-
-  // V1 Routes
-  router.post(
-    '/v1/secrets/share',
-    v1.secrets.validate('share'),
-    v1.secrets.share
-  )
-
-  router.post(
-    '/v1/secrets/combine',
-    v1.secrets.validate('combine'),
-    v1.secrets.combine
-  )
-
-  router.post(
-    '/v1/shards/verify',
-    v1.shards.validate('verify'),
-    v1.shards.verify
-  )
-
-  // V2 Routes
-  router.post(
-    '/v2/secrets/share',
-    v2.secrets.validate('share'),
-    v2.secrets.share
-  )
-
-  router.post(
-    '/v2/secrets/combine',
-    v2.secrets.validate('combine'),
-    v2.secrets.combine
-  )
-
-  router.post(
-    '/v2/secrets/verify',
-    v2.secrets.validate('verify'),
-    v2.secrets.verify
-  )
-
-  router.post(
-    '/v2/shards/verify',
-    v2.shards.validate('verify'),
-    v2.shards.verify
-  )
+  router.post('/secrets/share', controllers.secrets.validate('share'), controllers.secrets.share)
+  router.post('/secrets/combine', controllers.secrets.validate('combine'), controllers.secrets.combine)
+  router.post('/secrets/verify', controllers.secrets.validate('verify'), controllers.secrets.verify)
+  router.post('/shards/verify', controllers.shards.validate('verify'), controllers.shards.verify)
 }
 
