112281c73ff5cb03385165ec5bd9f6eeff8e00c9
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-01-30 14:12:45 +0000

    add swaggerUI

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
 
 const Controllers = require('./controllers/')
 const Routes = require('../config/routes')
@@ -13,5 +15,6 @@ app.use(bodyParser.json())
 const routes = Routes(router, Controllers())
 
 app.use('/', router)
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 exports = module.exports = app
