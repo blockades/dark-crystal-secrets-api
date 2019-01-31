9611b52a835924cb69f6bb1d28e5db695b449393
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-01-31 10:05:17 +0000

    re-add v1 for the API version (not the shard version...)

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 module.exports = (router, controllers) => {
-  router.post('/secrets/share', controllers.secrets.validate('share'), controllers.secrets.share)
-  router.post('/secrets/combine', controllers.secrets.validate('combine'), controllers.secrets.combine)
-  router.post('/secrets/verify', controllers.secrets.validate('verify'), controllers.secrets.verify)
-  router.post('/shards/verify', controllers.shards.validate('verify'), controllers.shards.verify)
+  router.post('/v1/secrets/share', controllers.secrets.validate('share'), controllers.secrets.share)
+  router.post('/v1/secrets/combine', controllers.secrets.validate('combine'), controllers.secrets.combine)
+  router.post('/v1/secrets/verify', controllers.secrets.validate('verify'), controllers.secrets.verify)
+  router.post('/v1/shards/verify', controllers.shards.validate('verify'), controllers.shards.verify)
 }
 
